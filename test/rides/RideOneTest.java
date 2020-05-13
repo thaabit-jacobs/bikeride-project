9b8514d448c71825a504cbd492b9c2b333a460cf
Mohammed Thaabit <51443211+thaabit-jacobs@users.noreply.github.com>
2020-05-13 15:07:22 +0200

    text format

--- test/rides/RideOneTest.java
+++ test/rides/RideOneTest.java
@@ -5,68 +5,66 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import bicycles.BicycleType;
-import bicycles.models.MountainBike;
-import bicycles.models.RoadBike;
-import bicycles.models.Tandem;
+import bicycles.models.*;
 import bicycles.rides.BikeRideOne;
-import bicycles.specifications.BicycleFromSpec;
-import bicycles.specifications.BicycleSpecification;
+import bicycles.specifications.*;
+
 
 public class RideOneTest {
 	
-	@Test
+    @Test
     public void RideOneTestMountainBike() {
-		MountainBike mb = new MountainBike();
-		BikeRideOne bro = new BikeRideOne(mb);	
-		bro.ride(mb);
-		assertEquals(4, mb.currentSpeed());
+	MountainBike mb = new MountainBike();
+	BikeRideOne bro = new BikeRideOne(mb);	
+	bro.ride(mb);
+	assertEquals(4, mb.currentSpeed());
     }
  
-	@Test
+    @Test
     public void RideOneTestRoadBike() {
-	 	RoadBike rb = new RoadBike();
-		BikeRideOne bro = new BikeRideOne(rb);	
-		bro.ride(rb);
-		assertEquals(14, rb.currentSpeed());
+	RoadBike rb = new RoadBike();
+	BikeRideOne bro = new BikeRideOne(rb);	
+	bro.ride(rb);
+	assertEquals(14, rb.currentSpeed());
     }
  
-	@Test
+    @Test
     public void RideOneTestTandem() {
-	 	Tandem t = new Tandem();
-		BikeRideOne bro = new BikeRideOne(t);	
-		bro.ride(t);
-		assertEquals(10, t.currentSpeed());
+	Tandem t = new Tandem();
+	BikeRideOne bro = new BikeRideOne(t);	
+	bro.ride(t);
+	assertEquals(10, t.currentSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecRideRoadBike() {
-		BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
-		BicycleFromSpec bikespec = new BicycleFromSpec(roadb);
-		BikeRideOne bikeride = new BikeRideOne(bikespec);
+	BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
+	BicycleFromSpec bikespec = new BicycleFromSpec(roadb);
+	BikeRideOne bikeride = new BikeRideOne(bikespec);
 		
-		bikeride.ride(bikespec);
+	bikeride.ride(bikespec);
 		
         assertEquals(14, bikeride.getCurrentSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecRideMountainbike() {
-		BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
-		BicycleFromSpec bikespec = new BicycleFromSpec(mountainb);
-		BikeRideOne bikeride = new BikeRideOne(bikespec);
+	BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
+	BicycleFromSpec bikespec = new BicycleFromSpec(mountainb);
+	BikeRideOne bikeride = new BikeRideOne(bikespec);
 		
-		bikeride.ride(bikespec);
+	bikeride.ride(bikespec);
 		
         assertEquals(4, bikeride.getCurrentSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecRideTandem() {
-		BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
-		BicycleFromSpec bikespec = new BicycleFromSpec(tandemb);
-		BikeRideOne bikeride = new BikeRideOne(bikespec);
+	BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
+	BicycleFromSpec bikespec = new BicycleFromSpec(tandemb);
+	BikeRideOne bikeride = new BikeRideOne(bikespec);
 		
-		bikeride.ride(bikespec);
+	bikeride.ride(bikespec);
 		
         assertEquals(10, bikeride.getCurrentSpeed());
     }
