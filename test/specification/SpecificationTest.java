894f01abb7742ad981d90991d92250b68cd879a1
Mohammed Thaabit <51443211+thaabit-jacobs@users.noreply.github.com>
2020-05-13 15:17:37 +0200

    text format

--- test/specification/SpecificationTest.java
+++ test/specification/SpecificationTest.java
@@ -8,68 +8,68 @@ import bicycles.rides.*;
 
 public class SpecificationTest {
 	
-	@Test
+    @Test
     public void bikeSpecNameRoadbike() {
-		BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
+	BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
 
         assertEquals(true, "Road bike".equals(roadb.getBikeType().getBikeName()));
     }
 	
-	@Test
+    @Test
     public void bikeSpecNameMountainbike() {
-		BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
+	BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
 	
         assertEquals(true, "MountainBike".equals(mountainb.getBikeType().getBikeName()));
     }
 	
-	@Test
+    @Test
     public void bikeSpecNameTandem() {
-		BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
+	BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
 	
         assertEquals(true, "Tandem".equals(tandemb.getBikeType().getBikeName()));
     }
 	
-	//
-	@Test
+	
+    @Test
     public void bikeSpecSpeedRoadbike() {
-		BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
+	BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
 	
         assertEquals(11, roadb.getBikeType().getAccelerationSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecSpeedMountainbike() {
-		BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
+	BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
 
         assertEquals(5, mountainb.getBikeType().getAccelerationSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecSpeedTandem() {
-		BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
+	BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
 		
         assertEquals(12, tandemb.getBikeType().getAccelerationSpeed());
     }
 	
-	//////
+
 	
-	@Test
+    @Test
     public void bikeSpecBrakeRoadbike() {
-		BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
+	BicycleSpecification roadb = new BicycleSpecification(BicycleType.ROAD_BIKE);
 	
         assertEquals(4, roadb.getBikeType().getBrakeSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecBrakeMountainbike() {
-		BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
+	BicycleSpecification mountainb = new BicycleSpecification(BicycleType.MOUNTAIN_BIKE);
 
         assertEquals(3, mountainb.getBikeType().getBrakeSpeed());
     }
 	
-	@Test
+    @Test
     public void bikeSpecBrakeTandem() {
-		BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
+	BicycleSpecification tandemb = new BicycleSpecification(BicycleType.TANDEM);
 		
         assertEquals(7, tandemb.getBikeType().getBrakeSpeed());
     }
